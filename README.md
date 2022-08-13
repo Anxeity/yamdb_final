0796f09c91f8a7ccbc0ed5f86fa583174c8e5543
Михаил Глазов <glazov_52@mail.ru>
2022-08-13 16:22:57 +0300

    FirstPush

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# yamdb_final
-yamdb_final
+# Проект yamdb_final
+Описание ...
+
+## Статус workflow
+[![workflow](https://github.com/anxeity/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg?branch=master)](https://github.com/anxeity/yamdb_final/actions/workflows/yamdb_workflow.yml)
---
+++ api_yamdb/core/management/commands/add_all_csv_files.py
@@ -0,0 +1,29 @@
+import os
+
+from django.core.management.base import BaseCommand
+
+from api_yamdb.settings import BASE_DIR
+
+from ._models_parser_func import (category_parser, comments_parser,
+                                  genre_parser, genre_title_parser,
+                                  review_parser, titles_parser, users_parser)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        parser_dict = {
+            'users': users_parser,
+            'category': category_parser,
+            'genre': genre_parser,
+            'titles': titles_parser,
+            'genre_title': genre_title_parser,
+            'review': review_parser,
+            'comments': comments_parser,
+        }
+
+        for name_csv, parser in parser_dict.items():
+            path = os.path.join(BASE_DIR, f'static/data/{name_csv}.csv')
+            parser_object = parser_dict[name_csv]
+            parser_object(path)
